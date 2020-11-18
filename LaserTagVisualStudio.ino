32bea176b1beccd9e567a8e50ae48203d807bd53
Francisco Arevalo <arevalofrancisco77@gmail.com>
2020-11-17 17:16:49 -0800

    ammo reload and game over
    Added ability to reload the weapon over time. Ammo is recharged about every second. Need to figure out how health will work and how it will be given back. This will likely cause issues with everything. Might have to do a simple reload button and leave the charging over time for the health. I am also considering adding a gun class so I can have multiple settings for guns. I also need to rework the general structure of the code. As it stands right now it feels like there isn't much structure being followed. I would like to keep the loop() function as clean as possible. The checkHit() function needs to mirror the checkAmmo

--- LaserTagVisualStudio.ino
+++ LaserTagVisualStudio.ino
@@ -7,25 +7,20 @@ const int laserPin = 13;
 enum Note {
     A = 440, B = 494, C = 262, D = 294, E = 330, F = 349, G = 392
 };
-
 class Player {
 private:
     int health = 10;
-    int ammo = 5;
+    int ammo = 10;
     bool alive = true;
 public:
-    void increaseAmmo(int amount) { ammo > 10 ? 10 : ammo += amount; }
-    void increaseHealth(int amount) { health > 10 ? 10 : health += amount; }
-
-    void decreaseHealth(int amount) { health <= 0 ? alive = false : health -= amount; }
-    void decreaseAmmo(int amount) { ammo <= 0 ? ammo = 0 : ammo -= amount; }
-
-    int setAlive(bool living) { alive = living; }
+    void setAmmo(int amount) { ammo > 10 ? ammo = 10 : ammo += amount; }
+    void setHealth(int amount) { health > 10 ? health = 10 : health += amount; }
+    void setAlive(bool living) { alive = living; }
+    
     int getHealth() { return health; }
     int getAmmo() { return ammo; }
     bool getAlive() { return alive; }
 } player1;
-
 void beep(Note freq1, Note freq2)
 {
     tone(piezoPin, freq1);
@@ -34,27 +29,37 @@ void beep(Note freq1, Note freq2)
     delay(100);
     noTone(piezoPin);
 }
-void reset(Player p1)
+void reset(Player& p1)
 {
-    p1.increaseHealth(10);
-    p1.increaseAmmo(5);
+    p1.setHealth(10);
+    p1.setAmmo(5);
     p1.setAlive(true);
-    Serial.println("You have been reset. /n Health: 10 /n Ammo: 5 /n Alive = True");
+    Serial.println("You have been reset.");
+    Serial.println("Health: 10 | Ammo: 5 | Alive = True");
 }
-
-void shootLaser()
+void reload(Player& p1)
 {
-    if (digitalRead(buttonPin) == LOW)
+    while (p1.getAmmo() < 10)
     {
-        digitalWrite(laserPin, HIGH);
-        beep(A, F);
-        digitalWrite(laserPin, LOW);
-        delay(200);
+        if (digitalRead(buttonPin) == LOW)
+            break;
+        p1.setAmmo(1);
+        Serial.print("Ammo: ");
+        Serial.println(p1.getAmmo());
+        beep(B, C);
+        delay(1000);
     }
-    else
-        digitalWrite(laserPin, LOW);
 }
-
+void shoot(Player& p1)
+{
+    p1.setAmmo(-1);
+    Serial.print("Ammo: ");
+    Serial.println(p1.getAmmo());
+    digitalWrite(laserPin, HIGH);
+    beep(A, F);
+    digitalWrite(laserPin, LOW);
+    delay(200);
+}
 void checkHit(Player& p1)
 {
     lightLVL = analogRead(A0);
@@ -62,29 +67,33 @@ void checkHit(Player& p1)
     {
         beep(G, A);
         digitalWrite(redLEDPin, HIGH); 
-        p1.decreaseHealth(1);
-        Serial.print("Current Health: ");
+        p1.setHealth(-1);
+        if (p1.getHealth() == 0)
+            p1.setAlive(false);
+        delay(1000);
+        Serial.print("HP: ");
         Serial.println(p1.getHealth());
     }
     else
         digitalWrite(redLEDPin, LOW);
 }
-
+void checkAmmo(Player& p1)
+{
+    if (digitalRead(buttonPin) == LOW && p1.getAmmo() > 0)
+        shoot(p1);
+    else if (digitalRead(buttonPin) == LOW && p1.getAmmo() <= 0)
+        beep(F, F);
+    else 
+        reload(p1);
+}
 void loop()
 {
-    while (player1.getAlive())
-    {
-        checkHit(player1);
-        shootLaser();
-    }
+    checkHit(player1);
+    checkAmmo(player1);
 
-    if (player1.getAlive() == false)
-    {
-        Serial.print("Game Over! Health reached zero.");
+    if (!player1.getAlive())
         reset(player1);
-    }
 }
-
 void setup()
 {
     Serial.begin(9600);
